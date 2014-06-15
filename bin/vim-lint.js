885f61fc32a4d77ccb61440ee646eb3f91998a92
Kuniwak <orga.chem.job@gmail.com>
2014-06-15 23:37:51 +0900

    Accept cmdline args

--- bin/vim-lint.js
+++ bin/vim-lint.js
@@ -6,17 +6,7 @@ var getEnabledPolicies = require('../lib/config.js').getEnabledPolicies;
 var SeverityLevel = require('../lib/level.js').SeverityLevel;
 var lint = require('../lib/lint.js').lint;
 
-var filepath = '/Users/OrgaChem/.vimrc';
-
-parseFile(filepath, function(err, ast) {
-  if (err) throw err;
-
-  var env = { path: filepath };
-  var policies = getEnabledPolicies(SeverityLevel.REFACTOR);
-
-  var violations = lint(policies, ast, env);
-  report(violations);
-});
+var filepath = process.argv[1];
 
 function parseFile(file, callback) {
   fs.readFile(file, 'utf-8', function(err, content) {
@@ -34,3 +24,13 @@ function report(violations) {
   var reporter = new Reporter();
   console.log(reporter.formatAll(violations));
 }
+
+parseFile(filepath, function(err, ast) {
+  if (err) throw err;
+
+  var env = { path: filepath };
+  var policies = getEnabledPolicies(SeverityLevel.REFACTOR);
+
+  var violations = lint(policies, ast, env);
+  report(violations);
+});
