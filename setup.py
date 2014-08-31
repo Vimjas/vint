6f59fb8dfeb5a4cdf2312299c4adfc5acc99c09e
Kuniwak <orga.chem.job@gmail.com>
2014-08-31 18:59:30 +0900

    Start coveralls

--- setup.py
+++ setup.py
@@ -2,14 +2,37 @@
 
 from setuptools import setup, find_packages
 
-setup(name='vint',
-      version='0.0',
-      description='Lint tool for Vim script Language',
-      author='Kuniwak (Orga Chem)',
-      author_email='orga.chem.job+vint@gmail.com',
-      url='https://github.com/Kuniwak/vint',
-      license='MIT',
-      install_requires=[
-          'PyYAML',
-      ],
-      packages=find_packages())
+
+def load_requires_from_file(filepath):
+    return [pkg_name.rstrip('\r\n') for pkg_name in open(filepath).readlines()]
+
+
+def install_requires():
+    return load_requires_from_file('requirements.txt')
+
+
+def test_requires():
+    return load_requires_from_file('test-requirements.txt')
+
+
+setup(
+    name='vint',
+    version='0.0',
+    description='Lint tool for Vim script Language',
+    author='Kuniwak',
+    author_email='orga.chem.job+vint@gmail.com',
+    url='https://github.com/Kuniwak/vint',
+    install_requires=install_requires(),
+    tests_require=test_requires(),
+    packages=find_packages(),
+    classfiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Topic :: Software Development :: Quality Assurance',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Text Editors',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
