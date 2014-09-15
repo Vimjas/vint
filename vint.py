312bc0bdd6a0c7db8e617f0051638a713fdf5bfb
Kuniwak <yuki.kokubun@mixi.co.jp>
2014-09-15 17:02:28 +0900

    Implement CLI

---
+++ vint.py
@@ -0,0 +1,6 @@
+import sys
+import vint
+
+
+if __name__ == '__main__':
+    vint.main(sys.argv[1:])
---
+++ vint/__init__.py
@@ -0,0 +1,39 @@
+from vint.linting.linter import Linter
+from vint.linting.formatter.formatter import Formatter
+from vint.linting.env import build_environment
+from vint.linting.config.config_container import ConfigContainer
+from vint.linting.config.config_cmdargs_source import ConfigCmdargsSource
+from vint.linting.config.config_default_source import ConfigDefaultSource
+from vint.linting.config.config_global_source import ConfigGlobalSource
+from vint.linting.config.config_project_source import ConfigProjectSource
+from vint.linting.policy_set import PolicySet
+
+
+def build_config_dict(env):
+    config = ConfigContainer(
+        ConfigDefaultSource(env),
+        ConfigGlobalSource(env),
+        ConfigProjectSource(env),
+        ConfigCmdargsSource(env),
+    )
+
+    return config.get_config_dict()
+
+
+def main(argv):
+    env = build_environment(argv)
+    paths_to_lint = env['file_paths']
+
+    policy_set = PolicySet()
+    config_dict = build_config_dict(env)
+
+    linter = Linter(policy_set, config_dict)
+    violations = []
+
+    for file_path in paths_to_lint:
+        violations += linter.lint_file(file_path)
+
+    formatter = Formatter(env)
+    output = formatter.format_violations(violations)
+
+    print(output)
