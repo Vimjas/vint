6722e16aef43f9cfe03e7e76fc578582139721f6
Kuniwak <yuki.kokubun@mixi.co.jp>
2014-12-20 18:11:27 +0900

    Split file collecting algorithm to FileFilter

--- vint/linting/env.py
+++ vint/linting/env.py
@@ -1,10 +1,7 @@
 import os
 import os.path
-import re
-import logging
 from pathlib import Path
-
-VIM_SCRIPT_FILE_NAME_PATTERNS = r'(?:[\._]g?vimrc|.*\.vim$)'
+from vint.linting.file_filter import find_vim_script
 
 
 def build_environment(cmdargs):
@@ -28,29 +25,6 @@ def _get_file_paths(cmdargs):
     if 'files' not in cmdargs:
         return []
 
-    found_files = _collect_files([Path(path) for path in cmdargs['files']])
-    return found_files
+    found_file_paths = find_vim_script(map(Path, cmdargs['files']))
 
-
-def _collect_files(paths):
-    result = set()
-    for path in paths:
-        if path.is_dir():
-            dir_path = path
-            result |= _collect_files(tuple(dir_path.iterdir()))
-
-        elif _is_vim_script(path):
-            file_path = path
-            result.add(file_path)
-
-        else:
-            logging.debug('ignore not Vim script file: `{file_path}`'.format(
-                file_path=str(path)))
-
-    return result
-
-
-def _is_vim_script(path):
-    file_name = path.name
-
-    return bool(re.search(VIM_SCRIPT_FILE_NAME_PATTERNS, file_name))
+    return set(found_file_paths)
