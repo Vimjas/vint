34bfb4dbb87be2e4a6af3c05ae1ea6614c86006c
Kuniwak <yuki.kokubun@mixi.co.jp>
2014-12-23 22:01:28 +0900

    Implement spec for severity

--- vint/linting/policy_set.py
+++ vint/linting/policy_set.py
@@ -1,29 +1,30 @@
 import logging
-from vint.linting.policy_registry import get_policy_class_map
+from vint.linting.level import is_level_enabled
+import vint.linting.policy_registry as policy_registry
 
 
 class PolicySet(object):
     def __init__(self):
-        self._all_policies = self._create_all_policies()
+        self._all_policies_map = self._create_all_policies_map()
         self.enabled_policies = []
 
 
-    def _create_all_policies(self):
-        policy_class_map = get_policy_class_map()
+    def _create_all_policies_map(self):
+        policy_class_map = policy_registry.get_policy_class_map()
 
-        policy_map = dict([(policy_name, PolicyClass())
-                           for policy_name, PolicyClass
-                           in policy_class_map.items()])
+        policy_map = {policy_name: PolicyClass()
+                      for policy_name, PolicyClass
+                      in policy_class_map.items()}
 
         return policy_map
 
 
     def _is_policy_exists(self, name):
-        return name in self._all_policies
+        return name in self._all_policies_map
 
 
     def _get_policy(self, name):
-        return self._all_policies[name]
+        return self._all_policies_map[name]
 
 
     def _warn_unexistent_policy(self, policy_name):
@@ -31,8 +32,30 @@ class PolicySet(object):
             name=policy_name))
 
 
-    def update_by_config(self, policy_enabling_map):
-        """ Update policies set by the policy enabling map.
+    def _log_severity(self, config_dict):
+        level_name = config_dict['cmdargs']['severity'].name
+        logging.debug('severity: {level}'.format(level=level_name))
+
+
+    def _get_enabling_map(self, config_dict):
+        self._log_severity(config_dict)
+
+        severity = config_dict['cmdargs']['severity']
+        policy_enabling_map = {}
+
+        for policy_name, policy in self._all_policies_map.items():
+            policy_enabling_map[policy_name] = is_level_enabled(policy.level, severity)
+
+        prior_policy_enabling_map = config_dict['policies']
+
+        for policy_name, policy in prior_policy_enabling_map.items():
+            policy_enabling_map[policy_name] = policy['enabled']
+
+        return policy_enabling_map
+
+
+    def update_by_config(self, config_dict):
+        """ Update policies set by the config dictionary.
 
         Expect the policy_enabling_map structure to be (represented by YAML):
             - PolicyFoo:
@@ -41,14 +64,13 @@ class PolicySet(object):
               enabled: False
               additional_field: 'is_ok'
         """
+        policy_enabling_map = self._get_enabling_map(config_dict)
         self.enabled_policies = []
 
-        for policy_name, policy_config in policy_enabling_map.items():
+        for policy_name, is_policy_enabled in policy_enabling_map.items():
             if not self._is_policy_exists(policy_name):
                 self._warn_unexistent_policy(policy_name)
                 continue
-
-            is_policy_enabled = policy_config['enabled']
 
             if is_policy_enabled:
                 enabled_policy = self._get_policy(policy_name)
