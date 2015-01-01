01e01f261b0babc375d320702acb2ddb6fb12c9d
Kuniwak <yuki.kokubun@mixi.co.jp>
2015-01-01 15:46:22 +0900

    Fix importing bug

--- vint/ast/plugin/scope_plugin/__init__.py
+++ vint/ast/plugin/scope_plugin/__init__.py
@@ -1,5 +1,7 @@
 from vint.ast.plugin.scope_plugin.reference_reachability_tester import (
     ReferenceReachabilityTester as ScopePlugin,
-    REACHABILITY_FLAG,
-    REFERECED_FLAG,
+    is_reference_identifier,
+    is_reachable_reference_identifier,
+    is_declarative_identifier,
+    is_referenced_declarative_identifier,
 )
