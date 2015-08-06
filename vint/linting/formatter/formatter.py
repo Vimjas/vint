22590f5ae954a1c003d1a32cd4d9046404b861a4
Kuniwak <yuki.kokubun@mixi.co.jp>
2015-08-06 15:32:58 +0900

    Care None value

--- vint/linting/formatter/formatter.py
+++ vint/linting/formatter/formatter.py
@@ -23,12 +23,12 @@ class Formatter(object):
         else:
             cmdargs = {}
 
-        if 'format' in cmdargs:
+        if cmdargs['format']:
             self._format = cmdargs['format']
         else:
             self._format = DEFAULT_FORMAT
 
-        if 'color' in cmdargs:
+        if cmdargs['color']:
             self._should_be_colorized = cmdargs['color']
         else:
             self._should_be_colorized = False
