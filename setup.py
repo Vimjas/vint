e7208b7223e63e6ee0f94fb22de6fc81669f3f30
Kuniwak <orga.chem.job@gmail.com>
2017-08-22 14:15:37 +0900

    Install typing when Python is older than 3.6

--- setup.py
+++ setup.py
@@ -15,6 +15,11 @@ def install_requires():
         requires.append('enum34 >= 1.0.4')
         # To enable pathlib in Python < 3.4
         requires.append('pathlib == 1.0.1')
+
+    if sys.version_info < (3, 6):
+        # To enable typing in Python < 3.6
+        requires.append('typing >= 3.6.2')
+
     return requires
 
 
