343c9b91e2112f5baa260e69fffe7f5bba3360dc
Kuniwak <orga.chem.job@gmail.com>
2017-11-04 21:48:40 +0900

    Drop pypy support

--- setup.py
+++ setup.py
@@ -63,7 +63,6 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
     ],
     entry_points={
         'console_scripts': [
