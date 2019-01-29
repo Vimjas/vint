219b733e1be2beb2f714088d3916fdb065f9abdc
Daniel Hahler <github@thequod.de>
2019-01-29 10:50:03 +0100

    Use yaml.safe_load (#298)
    Ref: https://github.com/Kuniwak/vint/issues/279#issuecomment-458106093

--- vint/linting/config/config_file_source.py
+++ vint/linting/config/config_file_source.py
@@ -13,7 +13,8 @@ class ConfigFileSource(ConfigSource):
         config_file_path = self.get_file_path(env)
 
         with config_file_path.open() as file_obj:
-            self._config_dict = self.convert_config_dict(yaml.load(file_obj))
+            config_yaml = yaml.safe_load(file_obj)
+            self._config_dict = self.convert_config_dict(config_yaml)
 
 
     def convert_config_dict(self, yaml_dict):
