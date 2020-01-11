3e6b4c1203978528c78c7e10c5da16df7ee18f7d
Daniel Hahler <git@thequod.de>
2020-01-11 20:16:22 +0100

    Drop support for py34
    It fails to install PyYAML on Travis:
    
    > Collecting PyYAML>=3.11 (from vim-vint==0.3.21.dev53+g930a103)
    >   Downloading https://files.pythonhosted.org/packages/3d/d9/ea9816aea31beeadccd03f1f8b625ecf8f645bd66744484d162d84803ce5/PyYAML-5.3.tar.gz (268kB)
    > ERROR: PyYAML requires Python '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*' but the running Python is 3.4.8

--- setup.py
+++ setup.py
@@ -59,7 +59,10 @@ setup(
         'Topic :: Text Editors',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
     ],
     entry_points={
